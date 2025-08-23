b23fea855dd752535ebb34317f2def12c863e5d4
ashy-ops <ashmitsinghsaggu2007@gmail.com>
2025-08-24 04:09:28 +0530

    feat: add orbital constants and earth parameters with SI units

--- include/constants.h
+++ include/constants.h
@@ -1,6 +1,18 @@
 #pragma once
 #include "project_includes.h"
 
-namespace constants{
 
+namespace constants {
+  // mathematical constants
+  extern const double pi; 
+  extern const double degreesToRadians;     
+  extern const double radiansToDegrees;
+
+  // time constants
+  extern const double secondsPerDay;  
+  
+  // earth's parameters
+  extern const double earthGravitationalParameter;
+  extern const double earthEquatorialRadius;
+  extern const double earthRotationRate;
 }
--- src/constants.cpp
+++ src/constants.cpp
@@ -1,5 +1,16 @@
 #include "constants.h"
 
-namespace constants{
-    // will be storing all the mathematical constants  here
+namespace constants {
+    // mathematical constants
+    const double pi = 3.141592653589793238462643383279502884;
+    const double degreesToRadians = pi / 180.0;
+    const double radiansToDegrees = 180.0 / pi;
+
+    // time constants
+    const double secondsPerDay = 86400.0;
+
+    //earth's physical constants
+    const double earthGravitationalParameter = 398600.4418e9;
+    const double earthEquatorialRadius = 6378137.0;
+    const double earthRotationRate = 7.2921150e-5;
 }
